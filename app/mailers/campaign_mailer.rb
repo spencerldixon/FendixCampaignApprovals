24010417e7bae79b5607164b025e32e67b28f719
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-09-16 12:05:45 +0100

    Added BCC

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -5,13 +5,13 @@ class CampaignMailer < ActionMailer::Base
   	@contact = contact
   	@message = message
   	@page = page
-  	mail(to: @contact.email, subject: "#{@page.name} Campaign Approval")
+  	mail(to: @contact.email, bcc: @page.user.email, subject: "#{@page.name} Campaign Approval")
   end
 
   def notify_admin_of_response(responses)
     @responses = responses
     @contact = responses.first.contact
   	@page = responses.first.approval_unit.page
-  	mail(to: @page.user.email, subject: "New Response Set for #{@page.name}")
+  	mail(to: @page.user.email, bcc: "spencer.dixon@fendixmedia.co.uk", subject: "New Response Set for #{@page.name}")
   end
 end
